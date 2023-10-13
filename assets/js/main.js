44ed308c72d4c9f945200df2861f448af4363357
lscon <lucimar.scon@gmail.com>
2023-10-13 19:01:14 -0300

    Criando card de detalhes para cada pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonInfo = document.getElementById('pokemon-info')
 
 const maxRecords = 151
 const limit = 10
@@ -7,7 +8,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onclick="pokemonModal('${pokemon.number}')">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -21,6 +22,34 @@ function convertPokemonToLi(pokemon) {
             </div>
         </li>
     `
+}
+
+$('.modal').on('hidden.bs.modal', function (e) {
+  $(this).empty();
+});
+
+function loadPokemonModalInfo(pokemon) {
+    return `
+
+    <div class="pokedex pokemon ${pokemon.type}">
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+        <div class="detail">
+            <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <ul class="statsModal">
+                Base Stats
+                ${pokemon.stats.map((stat) =>
+                    `<li class="statpokemon">${stat.stat.name} ${stat.base_stat}</li>`).join('')
+                }
+            </ul>
+        </div> 
+
+        <div class="btn-stats">
+            <button type="button" class="btn-default" data-dismiss="modal">Fechar</button>
+        </div>
+    </div>
+`
 }
 
 function loadPokemonItens(offset, limit) {
@@ -45,3 +74,15 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function pokemonModal(pokemon_number) {
+    console.log("Poke number " + pokemon_number)
+
+    request_pokemon_info(pokemon_number).then((pokemon) => {
+        const newHtml = loadPokemonModalInfo(pokemon);
+        console.log(pokemon);
+        $("#JanelaModal").append(newHtml);
+    });
+
+    $("#JanelaModal").modal();
+}
